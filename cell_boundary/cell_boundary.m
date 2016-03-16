cf6f0fac0be0bda4719f5c8abf1ef9cfee56b738
moosekaka <sweel@uci.edu>
2016-03-16 15:18:48 -0700

    image adjust

--- cell_boundary/cell_boundary.m
+++ cell_boundary/cell_boundary.m
@@ -1,5 +1,10 @@
+clear all; close all;
+
+fname = input('file name: ', 's');
+inputimage=single(uint8(imread(fname)/256));
 fillcell
 boundary
-normals3
-polarplot
-findcontour 
+normals3swee
+polarplotmanual2 %this is default
+findcontour
+outputcoord
